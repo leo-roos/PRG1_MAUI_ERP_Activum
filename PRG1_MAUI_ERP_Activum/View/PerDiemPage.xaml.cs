bed57a409f100fb7d8be2002fefb3f8b4b0d5a82
spiderlinus <linusschon08@gmail.com>
2026-01-21 14:44:46 +0100

    Added functionality for PerDiem calculation #19

--- PRG1_MAUI_ERP_Activum/View/PerDiemPage.xaml.cs
+++ PRG1_MAUI_ERP_Activum/View/PerDiemPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace PRG1_MAUI_ERP_Activum.View;
 
 public partial class PerDiemPage : ContentPage
@@ -6,4 +8,36 @@ public partial class PerDiemPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	int WholeDayPayment = 240;
+	int HalfDayPayment = 120;
+
+
+    private void CalculatePerDiem(object sender, EventArgs e)
+    {
+
+        int AmountOfWholeDays = 0;
+        int AmountOfHalfDays = 0;
+        int ExtraExpensesAmount = 0;
+
+        try
+		{
+			AmountOfWholeDays = int.Parse(WholeDays.Text);
+			AmountOfHalfDays = int.Parse(HalfDays.Text);
+			ExtraExpensesAmount = int.Parse(ExtraExpenses.Text);
+		}
+		catch
+		{
+			DisplayAlertAsync("Error", "Please enter a valid number", "Ok");
+		}
+
+		int WholeDayAmountResult = WholeDayPayment * AmountOfWholeDays;
+		int HalfDayAmountResult = HalfDayPayment * AmountOfHalfDays;
+		int TotalResult = HalfDayAmountResult + WholeDayAmountResult + ExtraExpensesAmount;
+
+		AllowanceResultLabel.Text = TotalResult + "kr";
+
+        ResultBorder.IsVisible = true;
+		
+    }
 }
