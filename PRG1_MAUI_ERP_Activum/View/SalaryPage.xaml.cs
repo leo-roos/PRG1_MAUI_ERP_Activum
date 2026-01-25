bc8c0f6415ed6eb195f63e28a242c3e320ef2e42
Leo Roos <leoroos88@gmail.com>
2026-01-25 17:39:33 +0100

    Add salary and semester salary calculation logic

--- PRG1_MAUI_ERP_Activum/View/SalaryPage.xaml.cs
+++ PRG1_MAUI_ERP_Activum/View/SalaryPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace PRG1_MAUI_ERP_Activum.View;
 
 public partial class SalaryPage : ContentPage
@@ -5,5 +7,72 @@ public partial class SalaryPage : ContentPage
 	public SalaryPage()
 	{
 		InitializeComponent();
+    }
+
+    readonly int baseSalary = 26000;
+    readonly int provisionRate = 12;
+    readonly int salaryTax = 32;
+
+    private int GetCalculatedSalary(int mySales)
+    {
+        double mySalesProvision = mySales * (provisionRate / 100.0);
+        double totalSalary = baseSalary + mySalesProvision;
+        double taxedSalary = totalSalary - (totalSalary * (salaryTax / 100.0));
+
+        return (int)taxedSalary;
+    }
+
+    private void CalculateSalary_Clicked(object sender, EventArgs e)
+    {
+        int mySales = 0;
+
+        if (string.IsNullOrEmpty(MySalesEntry.Text))
+        {
+            MySalesEntry.Focus();
+            DisplayAlertAsync("Error", "För att beräkna din Månadslön måste du ange din totala försäljning denna månad!", "Ok");
+            return;
+        }
+
+        if (int.TryParse(MySalesEntry.Text, out int result))
+        {
+            mySales = result;
+        }
+        else
+        {
+            MySalesEntry.Focus();
+            DisplayAlertAsync("Error", "För att beräkna din Månadslön måste du ange ett tal i \"din totala försäljning denna månad\"!", "Ok");
+            return;
+        }
+
+        int CalculatedSalary = GetCalculatedSalary(mySales);
+        ExpectedSalaryResult.Text = CalculatedSalary.ToString("N0", new CultureInfo("en-US")) + " kr";
+        ExpectedSalaryBorder.IsVisible = true;
+    }
+
+    private void CalculateSemesterSalary_Clicked(object sender, EventArgs e)
+    {
+        int myAverageSales = 0;
+
+        if (string.IsNullOrEmpty(MyAverageSalesEntry.Text))
+        {
+            MyAverageSalesEntry.Focus();
+            DisplayAlertAsync("Error", "För att beräkna din Månadslön måste du ange din totala försäljning denna månad!", "Ok");
+            return;
+        }
+
+        if (int.TryParse(MyAverageSalesEntry.Text, out int result))
+        {
+            myAverageSales = result;
+        }
+        else
+        {
+            MyAverageSalesEntry.Focus();
+            DisplayAlertAsync("Error", "För att beräkna din Semesterlön måste du ange ett tal i \"Genomsnittlig försäljning (11 månader)\"!", "Ok");
+            return;
+        }
+
+        int CalculatedSalary = GetCalculatedSalary(myAverageSales);
+        ExpectedSemesterSalaryResult.Text = CalculatedSalary.ToString("N0", new CultureInfo("en-US")) + " kr";
+        ExpectedSemesterSalaryBorder.IsVisible = true;
     }
 }
