b9202488310c53a76e14179e5ebbfa285b6182bc
Leo Roos <leoroos88@gmail.com>
2026-02-25 14:20:10 +0100

    handle selection clicks to view customer information

--- PRG1_MAUI_ERP_Activum/View/CustomersPage.xaml.cs
+++ PRG1_MAUI_ERP_Activum/View/CustomersPage.xaml.cs
@@ -1,19 +1,28 @@
 using PRG1_MAUI_ERP_Activum.Model;
+using PRG1_MAUI_ERP_Activum.Services;
 
 namespace PRG1_MAUI_ERP_Activum.View;
 
 public partial class CustomersPage : ContentPage
 {
-	public CustomersPage()
+    private readonly RegisterService _service = RegisterService.Instance;
+
+    public CustomersPage()
 	{
 		InitializeComponent();
 
-		//Customer customer = new Customer();
-
+        CustomersCollection.ItemsSource = _service.Customers;
     }
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private void Customers_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
+        var selected = e.CurrentSelection.FirstOrDefault() as Customer;
+        if (selected == null)
+        {
+            CustomerInsurances.ItemsSource = null;
+            return;
+        }
 
+        CustomerInsurances.ItemsSource = _service.GetInsurancesForCustomer(selected).ToList();
     }
 }
