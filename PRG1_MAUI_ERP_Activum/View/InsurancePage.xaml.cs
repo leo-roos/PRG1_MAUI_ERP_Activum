a819dce86f7363a7c7b3233ac4a26adb5eaa1d59
Leo Roos <leoroos88@gmail.com>
2026-03-04 13:34:14 +0100

    Revamp InsurancePage UI and data binding

--- PRG1_MAUI_ERP_Activum/View/InsurancePage.xaml.cs
+++ PRG1_MAUI_ERP_Activum/View/InsurancePage.xaml.cs
@@ -1,9 +1,45 @@
-namespace PRG1_MAUI_ERP_Activum.View;
+using PRG1_MAUI_ERP_Activum.Services;
+using PRG1_MAUI_ERP_Activum.Model;
+using System.Diagnostics;
 
+
+namespace PRG1_MAUI_ERP_Activum.View;
 public partial class InsurancePage : ContentPage
 {
-	public InsurancePage()
+    private readonly RegisterService _service = RegisterService.Instance;
+
+    public InsurancePage()
 	{
 		InitializeComponent();
+
+		Debug.WriteLine(_service.chosenCustomer);
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (_service.chosenCustomer == null)
+        {
+            CustomerInsurances.ItemsSource = null;
+            return;
+        }
+
+        InsurancesPicker.ItemsSource = _service.Insurances
+            .Where(i => _service.chosenCustomer.InsuranceId.Contains(i.Id))
+            .Select(i => $"{i.Type} ({i.MonthlyCost} kr / månad)")
+            .ToList();
+
+        CustomerInsurances.ItemsSource = _service.GetInsurancesForCustomer(_service.chosenCustomer).ToList();
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void CustomerInsurances_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
 }
