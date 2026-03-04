04eff52c97a46f1f4e55b2eb76103d80360ae764
Leo Roos <leoroos88@gmail.com>
2026-03-04 14:30:11 +0100

    Store Customer.Insurances

--- PRG1_MAUI_ERP_Activum/Services/RegisterService.cs
+++ PRG1_MAUI_ERP_Activum/Services/RegisterService.cs
@@ -36,11 +36,11 @@ namespace PRG1_MAUI_ERP_Activum.Services
             Customer customer1 = new Customer("Pelle", "Svanslös", "pelle@kattstugan.se", "0707 77 00 77");
             Customer customer2 = new Customer("Maja", "Gräddnos", "maja@katthotel.se", "0708 88 11 22");
 
-            customer1.InsuranceId.Add(insurance1.Id);
-            customer1.InsuranceId.Add(insurance2.Id);
-            customer1.InsuranceId.Add(insurance3.Id);
+            customer1.Insurances.Add(new Insurance(insurance1));
+            customer1.Insurances.Add(new Insurance(insurance2));
+            customer1.Insurances.Add(new Insurance(insurance3));
 
-            customer2.InsuranceId.Add(insurance3.Id);
+            customer2.Insurances.Add(new Insurance(insurance1));
 
             Customers.Add(customer1);
             Customers.Add(customer2);
@@ -49,18 +49,17 @@ namespace PRG1_MAUI_ERP_Activum.Services
         public Customer? GetCustomer(Guid id) => Customers.FirstOrDefault(c => c.Id == id);
         public Insurance? GetInsurance(Guid id) => Insurances.FirstOrDefault(i => i.Id == id);
 
-        public IEnumerable<Insurance> GetInsurancesForCustomer(Customer customer) =>
-            Insurances.Where(i => customer.InsuranceId.Contains(i.Id));
+        public IEnumerable<Insurance> GetInsurancesForCustomer(Customer customer) => customer.Insurances;
 
         public void LinkInsurance(Customer customer, Insurance insurance)
         {
-            if (!customer.InsuranceId.Contains(insurance.Id))
-                customer.InsuranceId.Add(insurance.Id);
+            if (!customer.Insurances.Contains(insurance))
+                customer.Insurances.Add(new Insurance(insurance));
         }
 
         public void UnlinkInsurance(Customer customer, Insurance insurance)
         {
-            customer.InsuranceId.Remove(insurance.Id);
+            customer.Insurances.Remove(insurance);
         }
     }
 }
