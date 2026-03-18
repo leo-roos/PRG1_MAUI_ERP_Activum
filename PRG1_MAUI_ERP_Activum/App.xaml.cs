38f18f08d65458ba906dfddde7a4111d176b78be
Leo Roos <leoroos88@gmail.com>
2026-03-18 12:47:39 +0100

    Set app width to 600

--- PRG1_MAUI_ERP_Activum/App.xaml.cs
+++ PRG1_MAUI_ERP_Activum/App.xaml.cs
@@ -21,7 +21,7 @@ namespace PRG1_MAUI_ERP_Activum
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            const double width = 400;
+            const double width = 600;
             const double height = 800;
             return new Window(new AppShell())
             {
