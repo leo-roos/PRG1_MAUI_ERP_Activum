4b6b83b521a10fea593ae62d0d7631d0caeffce2
Leo Roos <leoroos88@gmail.com>
2026-03-18 12:48:19 +0100

    Update RegisterService and add more test customers

--- PRG1_MAUI_ERP_Activum/Services/RegisterService.cs
+++ PRG1_MAUI_ERP_Activum/Services/RegisterService.cs
@@ -1,7 +1,6 @@
 ﻿
 using PRG1_MAUI_ERP_Activum.Model;
 using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace PRG1_MAUI_ERP_Activum.Services
 {
@@ -12,13 +11,11 @@ namespace PRG1_MAUI_ERP_Activum.Services
 
         public ObservableCollection<Customer> Customers { get; }
         public ObservableCollection<Insurance> Insurances { get; }
-        public Customer? chosenCustomer { get; set; }
 
         private RegisterService()
         {
             Customers = new ObservableCollection<Customer>();
             Insurances = new ObservableCollection<Insurance>();
-            chosenCustomer = null;
 
             TestData();
         }
@@ -35,6 +32,9 @@ namespace PRG1_MAUI_ERP_Activum.Services
 
             Customer customer1 = new Customer("Pelle", "Svanslös", "pelle@kattstugan.se", "0707 77 00 77");
             Customer customer2 = new Customer("Maja", "Gräddnos", "maja@katthotel.se", "0708 88 11 22");
+            Customer customer3 = new Customer("Erik", "Kattlös", "erik.kattlos@gmail.com", "0707 12 34 56");
+            Customer customer4 = new Customer("Lisa", "Kattälskare", "lisa@kattälskare.se", "0709 99 88 77");
+            Customer customer5 = new Customer("Olle", "Kattvän", "olle@kattvän.se", "0706 66 55 44");
 
             customer1.Insurances.Add(new Insurance(insurance1));
             customer1.Insurances.Add(new Insurance(insurance2));
@@ -42,22 +42,41 @@ namespace PRG1_MAUI_ERP_Activum.Services
 
             customer2.Insurances.Add(new Insurance(insurance1));
 
+            customer3.Insurances.Add(new Insurance(insurance2));
+            customer3.Insurances.Add(new Insurance(insurance3));
+
+            customer4.Insurances.Add(new Insurance(insurance1));
+            customer4.Insurances.Add(new Insurance(insurance2));
+
+            customer5.Insurances.Add(new Insurance(insurance3));
+
             Customers.Add(customer1);
             Customers.Add(customer2);
+            Customers.Add(customer3);
+            Customers.Add(customer4);
+            Customers.Add(customer5);
         }
 
-        public Customer? GetCustomer(Guid id) => Customers.FirstOrDefault(c => c.Id == id);
-        public Insurance? GetInsurance(Guid id) => Insurances.FirstOrDefault(i => i.Id == id);
+        public Customer? GetCustomer(Guid id)
+        {
+            return Customers.FirstOrDefault(c => c.Id == id);
+        }
 
-        public IEnumerable<Insurance> GetInsurancesForCustomer(Customer customer) => customer.Insurances;
+        public Insurance? GetInsurance(Guid id) {
+            return Insurances.FirstOrDefault(i => i.Id == id);
+        }
 
-        public void LinkInsurance(Customer customer, Insurance insurance)
+        public List<Insurance> GetInsurancesForCustomer(Customer customer) {
+            return customer.Insurances;
+        }
+
+        public static void LinkInsurance(Customer customer, Insurance insurance)
         {
             if (!customer.Insurances.Contains(insurance))
                 customer.Insurances.Add(new Insurance(insurance));
         }
 
-        public void UnlinkInsurance(Customer customer, Insurance insurance)
+        public static void UnlinkInsurance(Customer customer, Insurance insurance)
         {
             customer.Insurances.Remove(insurance);
         }
