1e43a79432df94142991832fa8edc4ef97c5ee1e
Leo Roos <leoroos88@gmail.com>
2026-04-15 14:28:52 +0200

    Add Date & EditDate to Note model

--- PRG1_MAUI_ERP_Activum/Model/Note.cs
+++ PRG1_MAUI_ERP_Activum/Model/Note.cs
@@ -4,5 +4,7 @@
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Message { get; set; } = "";
+        public DateTime Date { get; set; } = DateTime.Now;
+        public DateTime EditDate { get; set; } = DateTime.Now;
     }
 }
