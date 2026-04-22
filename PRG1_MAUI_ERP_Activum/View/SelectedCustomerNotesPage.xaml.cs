478537336cd840349eec31fbbdaede91291292a5
Leo Roos <leoroos88@gmail.com>
2026-04-22 13:36:28 +0200

    Add customer notes page and note model changes

---
+++ PRG1_MAUI_ERP_Activum/View/SelectedCustomerNotesPage.xaml.cs
@@ -0,0 +1,34 @@
+using PRG1_MAUI_ERP_Activum.Model;
+using PRG1_MAUI_ERP_Activum.Services;
+using System.Diagnostics;
+
+namespace PRG1_MAUI_ERP_Activum.View;
+
+public partial class SelectedCustomerNotesPage : ContentPage
+{
+    private readonly SelectedCustomerService selectedCustomerService = SelectedCustomerService.Instance;
+    private readonly RegisterService _service = RegisterService.Instance;
+
+    public SelectedCustomerNotesPage()
+	{
+		InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (selectedCustomerService.SelectedCustomer != null)
+        {
+            Title.Text = $"{selectedCustomerService.SelectedCustomer.FirstName} {selectedCustomerService.SelectedCustomer.LastName} - Anteckningar";
+
+            CustomerNotes.ItemsSource = null;
+            CustomerNotes.ItemsSource = _service.GetNotesForCustomer(selectedCustomerService.SelectedCustomer.Id);
+        }
+    }
+
+    private void GoBack_Clicked(object sender, EventArgs e)
+    {
+        selectedCustomerService.GotoPage();
+    }
+}
